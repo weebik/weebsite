29d99658b6dacd089507406c76340de2838977fc
weebik <krystian.samaa@gmail.com>
2024-12-01 14:55:12 +0100

    chore: responsive lander fix

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -13,7 +13,7 @@ function Footer() {
         backgroundImage: `url(${footerBackground})`,
         backgroundSize: "cover",
         width: "100%",
-        height: "150px",
+        height: "15vh",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
