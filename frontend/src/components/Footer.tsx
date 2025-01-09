aa14368b23d72d8e6df930c70f7d0763cad8231e
weebik <krystian.samaa@gmail.com>
2025-01-09 11:57:52 +0100

    chore: Changes to footer style

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,37 +1,19 @@
-import { Typography, Box } from "@mui/material";
-import footerBackground from "../assets/footer.svg";
+import { Typography } from "@mui/material";
 import "../styles/footer.css";
 import MediaLinks from "./MediaLinks";
 
 function Footer() {
   return (
-    <Box
-      sx={{
-        backgroundImage: `url(${footerBackground})`,
-        backgroundSize: "cover",
-        width: "100%",
-        height: "120px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: "auto",
-        zIndex: "1000",
-      }}
-    >
-      <Box
-        sx={{
-          color: "#fff",
-          width: "100%",
-        }}
-      >
+    <div className="footer">
+      <div className="footer-content">
         <div className="footer-divider">
           <Typography variant="overline">
             © 2024 Krystian Ćwikliński. All rights reserved.
           </Typography>
           <MediaLinks />
         </div>
-      </Box>
-    </Box>
+      </div>
+    </div>
   );
 }
 
