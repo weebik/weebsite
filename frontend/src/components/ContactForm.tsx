e66df8ec7d278752eb8f4ac05653181cc082acb8
weebik <krystian.samaa@gmail.com>
2025-02-05 22:02:13 +0100

    fix: cleanup

--- frontend/src/components/ContactForm.tsx
+++ frontend/src/components/ContactForm.tsx
@@ -10,6 +10,7 @@ import {
   validateEmail,
   submitContactForm,
 } from "../utils/formUtils";
+import { inputStyle } from "../consts/inputStyle";
 
 function ContactForm({ text, nameLabel, messageLabel }: ContactFormProps) {
   const [formData, setFormData] = useState({
@@ -67,27 +68,6 @@ function ContactForm({ text, nameLabel, messageLabel }: ContactFormProps) {
       }
     );
     setIsSubmitting(false);
-  };
-
-  const inputStyle = {
-    "& .MuiOutlinedInput-root": {
-      "& fieldset": {
-        border: "none",
-      },
-    },
-    "& .MuiInputBase-input": { color: "var(--text)" },
-    "& .MuiInputLabel-root": { color: "var(--sec2)" },
-
-    color: "white",
-    borderRadius: "0.5em",
-    resize: "none",
-    transition: "filter 300ms ease-in-out",
-    filter: "drop-shadow(0 0 0.2vw rgba(133, 163, 171, 0.2))",
-    background: "rgba(22, 34, 55, 1)",
-    "&:focus": {
-      outline: "none",
-      filter: "drop-shadow(0 0 0.3vw rgba(133, 163, 171, 0.4))",
-    },
   };
 
   return (
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,6 +1,1 @@
 /// <reference types="vite/client" />
-// typy w osobnych
-// axios zamiast fetch
-// react hook forms
-// uniwersalizacja bazy
-// mail provider
